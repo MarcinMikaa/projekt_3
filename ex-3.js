2361cd00388d8f8c48aeee87841417be83b0a9b4
Marcin <marcin.mika123@gmail.com>
2021-09-23 08:52:36 +0200

    Updated

--- ex-3.js
+++ ex-3.js
@@ -1,15 +1,13 @@
-function charCount(char, word) {
-    let count = 0;
-    for(let i = 0; i < word.length; i++) {
-        if(word[i] == char) {
-            count = count + 1;
-        }
-    }
-    return count;
-}
+// function charCount(char, word) {
+//     let count = 0;
+//     for(let i = 0; i < word.length; i++) {
+//         if(word[i] == char) {
+//             count = count + 1;
+//         }
+//     }
+//     return count;
+// }
 
-
-
-console.log(charCount("a", "easit"));
-console.log(charCount("c", "Chamber of secrets"));
-console.log(charCount("b", "big fat bubble"));
+// console.log(charCount("a", "easit"));
+// console.log(charCount("c", "Chamber of secrets"));
+// console.log(charCount("b", "big fat bubble"));
