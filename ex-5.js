093f8e45b931a6bef8c62c710d17574c063d4eac
Marcin <marcin.mika123@gmail.com>
2021-09-23 10:01:04 +0200

    Updated v2

--- ex-5.js
+++ ex-5.js
@@ -1,13 +1,10 @@
 function arrayOfMultiples(num, length) {
     const array = [];
     for(let i = 1; i <= length; i++) {
-        let m = num * i;
-        array.push(m);
+        array.push(num * i);
     }
     return array;
 }
-
-
 
 console.log(arrayOfMultiples(7, 5));
 console.log(arrayOfMultiples(12, 10));
