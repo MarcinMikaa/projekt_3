24a0e1fa71956b5263bae0e72291db158c31115c
Marcin <marcin.mika123@gmail.com>
2021-09-23 13:12:58 +0200

    Working

--- ex-6.js
+++ ex-6.js
@@ -1,28 +1,25 @@
 function danceConvert(pin) {
 
-    const array = ["Shimmy", "Shake", "Pirouette", "Slide", "Arabesque", "Pop"];
-    let sum = 0;
+    const array = ["Shimmy", "Shake", "Pirouette", "Slide", "", "", "", "Pop", "", "Arabesque"];
     const newPin = [];
 
     const helpArray = [...pin];
     const helpArray2 = helpArray.map(Number);
 
     for(let i = 0; i < helpArray2.length; ++i) {
+
         let help = helpArray2[i] + i;
+        let index = help % array.length;
+        newPin.push(array[index]);
 
-        if(help > array.length - 1) {
-            let a = array.pop();
-            array.unshift(a);
+        if(newPin[i] == "") {
+            return "Invalid input";
         }
-
-        newPin.push(array[help]);
     }
-
     return newPin;
-
 }
 
 console.log(danceConvert("0000"));
 console.log(danceConvert("3856"));
-// console.log(danceConvert("9999"));
-// console.log(danceConvert("32a1"));
+console.log(danceConvert("9999"));
+console.log(danceConvert("32a1"));
